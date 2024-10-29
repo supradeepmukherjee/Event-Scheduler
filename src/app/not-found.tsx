ff3177a38b8681eeb9c3beadce664c8612679bb7
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-10-29 11:19:41 +0530

    Not Found Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => (
+    <div className="text-4xl font-extrabold w-screen pt-80 grid justify-center">
+      <h1>
+        Error 404 - Page Not Found
+      </h1>
+    </div>
+)
+
+export default NotFound
