f6b89dcb1458e0a6484cb7e4fb5b05570c65c21f
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-11-04 20:45:50 +0530

    Get User Events Action

--- src/actions/events.ts
+++ src/actions/events.ts
@@ -17,3 +17,16 @@ export async function createEvent(data: Event) {
     })
     return { success: true, event }
 }
+
+export async function getUserEvents() {
+    const { userId } = auth()
+    if (!userId) throw new Error('Unauthorised')
+    const user = await prisma.user.findUnique({ where: { clerkUserID: userId } })
+    if (!user) throw new Error('User not Found')
+    const events = await prisma.event.findMany({
+        where: { userID: user.id },
+        orderBy: { createdAt: 'desc' },
+        include: { _count: { select: { bookings: true } } }
+    })
+    return { success: true, events, username: user.username }
+}
