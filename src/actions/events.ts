1c63efd1bcbc0afbd38643584dc015fa9b56762d
Supradeep Mukherjee <supradeep2004@gmail.com>
2024-11-04 21:09:38 +0530

    Delete Event Action

--- src/actions/events.ts
+++ src/actions/events.ts
@@ -30,3 +30,14 @@ export async function getUserEvents() {
     })
     return { success: true, events, username: user.username }
 }
+
+export async function delEvent(id: string) {
+    const { userId } = auth()
+    if (!userId) throw new Error('Unauthorised')
+    const user = await prisma.user.findUnique({ where: { clerkUserID: userId } })
+    if (!user) throw new Error('User not Found')
+    const event = await prisma.event.findUnique({ where: { id } })
+    if (!event || event.userID !== user.id) throw new Error('Event not found or unauthorised')
+    await prisma.event.delete({ where: { id } })
+    return { success: true }
+}
